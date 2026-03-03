ebd1a5878deb51fb03ec90147db2be65ff1b6158
svetakosheleva05 <lana.kosheleva05@mail.ru>
2026-03-03 11:14:48 +0300

    Create Person.java
    Создан класс Person

---
+++ yamlreader/src/main/java/com/mycompany/yamlreader/Person.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.yamlreader;
+
+/**
+ *
+ * @author svetl
+ */
+public class Person {
+    private String name;
+    private String adress;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+}
